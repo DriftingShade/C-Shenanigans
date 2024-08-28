c6997e1580a229e0ac7c4d6675d59e3c27b4311a
DriftingShade <sarus333@yahoo.com>
2024-08-28 16:46:26 -0600

    Learning User Input

---
+++ 01-language-essentials/UserInput/Program.cs
@@ -0,0 +1,12 @@
+﻿string? input;
+
+do
+{
+    Console.WriteLine("Type something, then hit enter:");
+    input = Console.ReadLine();
+    if (input != null && input.Trim().Length > 0)
+    {
+        Console.WriteLine($"You typed: {input}");
+    }
+} 
+while (input == null || input.Trim().Length == 0);
